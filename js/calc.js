b5803048b43452a384ec9693e50988007aec1666
Vladimir <vladimir052290@mail.ru>
2017-08-30 14:53:45 +0300

    bag fix calc js, css, html

--- js/calc.js
+++ js/calc.js
@@ -18,12 +18,12 @@ $(document).ready(function() {
 			$premiss = 0;																	
 		};
 		var $socket = $('select.socket').val(); 											//розетки
-		var $switch = $premiss;
-		var $point = 0; 
+		var $switch = $premiss;																//колличество выключателей
+		var $point = 0; 																	//колличество точек
 		var $ceiling_height = $('select.ceiling_height').val(); 							//извлечение и присваивание перемменной высоты потолка
-		var $electrical_box = $premiss * 2;
+		var $electrical_box = $premiss * 2;													//Объем щита
 		var	$cutout = $electrical_box + 2;													//колличество автоматов
-		var $electrical_board = $cutout;													//Величина щита
+		var $electrical_board = 0 ;															//Величина щита
 		var $wires = $room * 100;															//провода
 		var $electrical_chases_length = 0;
 		$electrical_box = parseInt($electrical_box);
@@ -41,14 +41,20 @@ $(document).ready(function() {
 		};
 		var $electrical_box_price = $electrical_box * 350;
 		var $electrical_board_price = 0;
-		if ( $electrical_board <= 12){
+		if ( $cutout <= 12){
 			$electrical_board_price = 1500;
-			}	else if ( 12 < $electrical_board <= 24){
+			$electrical_board = 12;}
+			/*else if ( 12 < $cutout <= 18){
+			$electrical_board_price = 2250;
+			$electrical_board = 18;}*/
+				else if ( 12 < $cutout <= 24){
 				$electrical_board_price = 3000;
-				} else if ( 24 < $electrical_board <= 36 ){
+				$electrical_board = 24;}
+					/*else if ( 24 < $cutout <= 36 ){
 					$electrical_board_price = 4500;
-		};
-		var	$commutation_electrical_board_price = $electrical_board * 110;													//колличество автоматов
+					$electrical_board = 36;
+		};*/
+		var	$commutation_electrical_board_price = $cutout * 110;													//колличество автоматов
 		var $electrical_chases_price = $electrical_chases_length / 100 * 110 + 800;
 		if ($electrical_chases_length == 0){
 			$electrical_chases_price = 0;
@@ -64,29 +70,37 @@ $(document).ready(function() {
 			$('span#final_price_advanced').text($summ_advanced);
 			$('td#final_price_advanced').text($summ_advanced + ' p.');
 		});
+
 		$('td.electrical_box').text($electrical_box);
 		$('td.electrical_box_price').text($electrical_box_price + ' p.');
+		
 		$('td.electrical_board').text($electrical_board);
 		$('td.electrical_board_price').text($electrical_board_price + ' p.');
+		
+		$('td.cutout').text($cutout);
 		$('td.commutation_electrical_board_price').text($commutation_electrical_board_price + ' p.');
+		
 		$('td.electrical_chases_length').text($electrical_chases_length / 100);
 		$('td.electrical_chases_price').text($electrical_chases_price + ' p.');
+		
 		$('td.wires').text($wires);
 		$('td.wires_price').text($wires_price + ' p.');
+		
 		$('td.point').text($point);
 		$('td.point_price').text($point_price + ' p.');
+		
 		if ($room  == 0) {
 			$('select.room').css("border-color", "red");
-		} else $('select.room').css("border-color", "green");
+			} else $('select.room').css("border-color", "green");
 		if ($bathroom  == 0) {
 			$('select.bathroom').css("border-color", "red");
-		} else $('select.bathroom').css("border-color", "green");
+			} else $('select.bathroom').css("border-color", "green");
 		if ($ceiling_height  == 0) {
 			$('select.ceiling_height').css("border-color", "red");
-		} else $('select.ceiling_height').css("border-color", "green");
+			} else $('select.ceiling_height').css("border-color", "green");
 		if ($socket  == 0) {
 			$('select.socket').css("border-color", "red");
-		} else $('select.socket').css("border-color", "green");
+			} else $('select.socket').css("border-color", "green");
 		/*console.log("точки " + $point);
 		console.log("длинна штроб " + $electrical_chases_length);
 		console.log("высота потолка " + $ceiling_height);*/
@@ -94,7 +108,7 @@ $(document).ready(function() {
 		var $summ = $softcalc * $price;
 		$('.hide').hide();*/
 		//console.log($electrical_box_price);
-		//console.log($electrical_board_price);
+		console.log($electrical_board);
 
 		$('#result').click(function(){
 			$('span#final_price').text($summ);
